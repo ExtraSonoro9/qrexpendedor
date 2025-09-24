49fef740abbd99877f8a7b7982fb3eb302ab57b5
Tomas Domingo <59489838+ExtraSonoro9@users.noreply.github.com>
2025-09-24 20:41:48 -0300

    Update py.py

--- py.py
+++ py.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 import threading
 
 app = Flask(__name__)
+import os
 
 # Función que simula activar motor en tu app Python
 def activar_motor_callback():
@@ -21,7 +22,10 @@ def webhook():
 def start_server():
     app.run(host="0.0.0.0", port=5000)
 
-# Correr el servidor en un hilo separado para que no bloquee Tkinterfgfgdhgdg
+    # Correr el servidor en 
+    if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
 
 
 
